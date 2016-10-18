96a9ba731a5fe26af16a33d74204554d49e024cd
fullhouse1 <godorgood@naver.com>
2016-10-18 11:45:13 +0900

    Add files via upload

---
+++ forbasic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAX 5
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= MAX; i++)
+	{
+		printf("C언어 재밌네요! %d\n", i);
+
+	}
+
+	printf("\n제어변수 i => %d\n", i);
+
+	return 0;
+}
